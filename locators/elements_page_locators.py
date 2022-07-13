3d5a2b03944263abfb8f5e20db8c116fa238d584
Anton <toxameosh@gmail.com>
2022-07-13 23:58:41 +0300

    added 'check_field_form'

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -2,8 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class TextBoxPageLocators:
-    FULL_NAME = (By.CSS_SELECTOR, "#userName")
-    EMAIL = (By.CSS_SELECTOR, "#userEmail")
+    FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
     CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
     PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
     SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -7,7 +7,14 @@ class TextBoxPage(BasePage):
 
     def fill_all_fields(self):
         self.element_is_visible(self.locators.FULL_NAME).send_keys('ddd')
-        self.element_is_visible(self.locators.EMAIL).send_keys('qwe@qwe.qwer')
+        self.element_is_visible(self.locators.EMAIL).send_keys('qwe@qwe.com')
         self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('qwe')
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('qwerq')
         self.element_is_visible(self.locators.SUBMIT).click()
+
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(":")[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(":")[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(":")[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(":")[1]
+        return full_name, email, current_address, permanent_address
